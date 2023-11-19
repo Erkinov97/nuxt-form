e3e49a591c301615ba724d4b3daa43aad34e3aa3
erkinov97 <erkinov997@gmail.com>
2023-11-19 23:12:00 +0500

    feat: added tailwindcss module and create layouts/pages/components folders setted default layout

---
+++ error.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+// 
+</script>
+
+<template>
+  <div>
+    <NuxtLink to="/">Home page</NuxtLink>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const route = useRoute();
+</script>
+
+<template>
+  <div>
+    <h1 class="text-red-600 text-3xl underline">Nuxt Routing set up successfully!</h1>
+  </div>
+</template>
